bc6cc3e8294911c0211ab3fa255344688e1c13fd
wjy-de <2291776153@qq.com>
2016-11-22 21:11:06 +0800

    js效果1

--- pages/my/myNews/myNews.js
+++ pages/my/myNews/myNews.js
@@ -1,16 +1,16 @@
 app.controller('myNewsCtrl',['$scope',function($scope){
 	
 }])
-//app.directive('ngN',[function(){
-//	return {
-//		restrict:"AE",
-//		link:function($scope,el){
-//				$(".xxk div").on("touchend",function(){
-//						var index=$(this).index();
-//						$(".xxk div").removeClass("active");
-//						$(this).addClass('active');
-//						$("ul").css("display","none").eq(index).css("display","block");
-//	            })
-//		}
-//	}
-//}])
+app.directive('ngN',[function(){
+	return {
+		restrict:"AE",
+		link:function($scope,el){
+				$(".xxk div").on("touchend",function(){
+						var index=$(this).index();
+						$(".xxk div").removeClass("active");
+						$(this).addClass('active');
+						$("ul").css("display","none").eq(index).css("display","block");
+	            })
+		}
+	}
+}])
