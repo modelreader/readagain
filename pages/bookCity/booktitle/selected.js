ac69eaddc1e89715935c7ed8f214ae060633056a
shine-xiaoyu <923577587@qq.com>
2016-11-22 19:07:46 +0800

    '书城3'

--- pages/bookCity/booktitle/selected.js
+++ pages/bookCity/booktitle/selected.js
@@ -1,3 +1,23 @@
 app.controller('booktitleCtrl',['$scope',function($scope){
 	
-}])
+}]);
+app.directive("ngXcase",function(){
+	return{
+		restrict:"A",
+		transclude:true,
+		replace:true,
+		template:'<div class="BookCity-list"><div ng-transclude></div></div>',
+		link:function($scope,el){
+			$('.dis').eq(0).css('display','block');
+			$('.BookCity-header').on('click','.classify',function(){
+				$('.dis').css('display','none');
+				var i=$(this).index();
+				console.log(i)
+				$('.dis').eq(i).css('display','block');
+				$(".classify").removeClass('back');
+				$(this).addClass('back');
+			})
+		}
+		
+	}
+})
