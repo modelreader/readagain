7dcd038342d52913ade7ce304ae7106d70c6350a
chenxi-chenjia <709207209@qq.com>
2016-11-22 19:31:38 +0800

    修改

--- pages/shelf/titleSelection/titleSelection.js
+++ pages/shelf/titleSelection/titleSelection.js
@@ -1,3 +1,30 @@
 app.controller('titleSelectionCtrl',['$scope',function($scope){
-	
+	$scope.hide=function(){
+		$("#titleSelection .header .hide").toggle();
+	}
+}])
+app.directive("ngChosetitle",[function(){
+	return {
+		restrict:"AE",
+		link:function($scope,el){
+			var el = el;
+			$(el).on("touchend",".click",function(){
+				$(el).find(".now").removeClass('now');
+				$(this).addClass('now');
+			})
+		}
+
+	}
+}])
+app.directive("ngChoselist",[function(){
+	return{
+		restrict:"AE",
+		link:function($scope,el){
+			$(el).find("a").attr("href","javascript:;")
+			$(el).on("touchend","li",function(){
+				$(el).find("li.now").removeClass('now');
+				$(this).addClass('now');
+			})
+		}
+	}
 }])
